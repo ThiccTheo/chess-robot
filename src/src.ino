bdfb6fe9bed0a37ac82aea02a39ac715a3934636
Theo Lee <th3o1237@gmail.com>
2025-05-31 14:52:19 -0700

    Update src.ino

--- src/src.ino
+++ src/src.ino
@@ -1,9 +1,18 @@
-void setup() {
-  // put your setup code here, to run once:
+// When button pressed, rotate servo from 0 to 180
+// When button released, rotate servo back to 0
 
+#include <Servo.h>
+
+Servo servo;
+
+void setup() {
+	servo.attach(9);
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
-
+	if (digitalRead(2) == HIGH) {
+		servo.write(180);
+	} else {
+		servo.write(0);
+	}
 }
