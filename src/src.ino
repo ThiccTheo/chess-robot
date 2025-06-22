e00fb81da7ed87ca0f4c5195fbc46f0d3e501ec6
Theo Lee <th3o1237@gmail.com>
2025-06-22 13:15:29 -0700

    construction of arm completed; added rotation function for slower rotation speeds to prevent jerk

--- src/src.ino
+++ src/src.ino
@@ -1,28 +1,40 @@
-// When button pressed, rotate servo from 0 to 180
-// When button released, rotate servo back to 0
-
 #include <Servo.h>
 #include <math.h>
 
 Servo base;
 Servo shoulder;
 Servo elbow;
-Servo wrist;
-Servo gripper;
 
 const bool ROBOT_COLOR = 0;
 
-float baseAngle(char file, int rank) {
+float base_angle(char file, int rank) {
 	float squareX = (ROBOT_COLOR == 0 ? 1 : -1) * (file - 96 - 4.5);
 	float squareY = (ROBOT_COLOR == 0 ? rank : 9 - rank) - 0.5;
 	return atan2(squareY, squareX);
 }
 
+void set_rotation(Servo* servo, int angle) {
+	while (servo->read() != angle) { 
+		servo->write(servo->read() + (angle - servo->read() > 0) - (angle - servo->read() < 0));
+		delay(100);
+	}
+}
+
 void setup() {
+	base.write(0);
+	shoulder.write(0);
+	elbow.write(0);
 	base.attach(12);
 	shoulder.attach(11);
 	elbow.attach(10);
 }
 
 void loop() {
+	set_rotation(&base, 180);
+	set_rotation(&base, 90);
+	set_rotation(&shoulder, 45);
+	set_rotation(&elbow, 90);
+	set_rotation(&elbow, 0);
+	set_rotation(&shoulder, 0);
+	set_rotation(&base, 0);
 }
